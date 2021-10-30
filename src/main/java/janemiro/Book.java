c1fa6d9277a374a97912fade3d73dd7b13a5a33f
Evgeniia Miroshina <evgeniia.miroshina@t-systems.com>
2021-10-30 19:06:02 +0300

    added classes

---
+++ src/main/java/janemiro/Book.java
@@ -0,0 +1,35 @@
+package janemiro;
+
+public class Book {
+    String name;
+    String author;
+    int amountOfPages;
+    boolean hardCover;
+    double paperDensity;
+
+    public void libraryCatalog() {
+        System.out.println(author + ". " + name + ". Страниц: " + amountOfPages + ". Плотность бумаги: " + paperDensity);
+    }
+
+    public static void main(String[] args) {
+        Book javaTextbook = new Book();
+
+        javaTextbook.author = "Кэти Сьерра";
+        javaTextbook.name = "изучаем java";
+        javaTextbook.hardCover = true;
+        javaTextbook.amountOfPages = 256;
+        javaTextbook.paperDensity = 1.47;
+
+        Book idiot = new Book();
+        idiot.author = "Ф. М. Достоевский";
+        idiot.name = "идиот";
+        idiot.hardCover = false;
+        idiot.amountOfPages = 500;
+        idiot.paperDensity = 2.23;
+
+        javaTextbook.libraryCatalog();
+        idiot.libraryCatalog();
+
+
+    }
+}
---
+++ src/main/java/janemiro/Cat.java
@@ -0,0 +1,38 @@
+package janemiro;
+
+public class Cat {
+    String name;
+    String sex;
+    int age;
+    String color;
+    String note;
+    boolean needsMedHelp;
+
+    public void shelterPromo() {
+        System.out.println("Знакомьтесь! Это " + sex + " " + name + ", возраст: " + age + ", цвет: " + color + ". Хотите взять домой? Правда, есть нюанс: " + note);
+    }
+
+    public static void main(String[] args) {
+        Cat murzik = new Cat();
+        murzik.name = "Мурзик";
+        murzik.age = 5;
+        murzik.sex = "котик";
+        murzik.color = "чёрный";
+        murzik.note = "нет одной лапки";
+        murzik.needsMedHelp = false;
+
+        Cat myshka = new Cat();
+        myshka.name = "Мышка";
+        myshka.age = 2;
+        myshka.sex = "кошечка";
+        myshka.color = "рыжий";
+        myshka.note = "покусали собаки";
+        myshka.needsMedHelp = true;
+
+        murzik.shelterPromo();
+        myshka.shelterPromo();
+
+
+    }
+
+}
